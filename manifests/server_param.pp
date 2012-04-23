5733c054086eeb667a2941ce23261053015cb9d3
Nathan Huff <nrhuff@umn.edu>
2012-04-23 15:41:00 -0500

    allow you to change different sections of the mysql config file

--- manifests/server_param.pp
+++ manifests/server_param.pp
@@ -1,11 +1,11 @@
-define mysql::server_param($value) {
+define mysql::server_param($value,$section='mysqld') {
   $conf  = $mysql::server::conf_file 
   $param = $name
 
   augeas{"mysql-$param":
     incl    => $conf,
     lens    => 'mysql.lns',
-    context => "/files${conf}/target[. = 'mysqld']",
+    context => "/files${conf}/target[. = '${section}']",
     changes => "set $param $value",
     notify  => Service['mysqld'],
     require => File[$conf],
