982b7ed20e14c2024d681eb9e4617936b6f079d9
Nathan Huff <nrhuff@umn.edu>
2012-04-23 13:35:44 -0500

    use path expression to lookup correct augeas path

--- manifests/server_param.pp
+++ manifests/server_param.pp
@@ -1,11 +1,10 @@
 define mysql::server_param($params) {
-	$param = $name
-	$value = $params[$name]
+  $param = $name
+  $value = $params[$name]
 
-	augeas{"mysql-$param":
-		context => '/files/etc/my.cnf/mysqld',
-		changes => "set $param $value",
-		notify  => Service['mysqld'],
-		require => File['/usr/share/augeas/lenses/dist/mysql.aug']
-	}
+  augeas{"mysql-$param":
+    context => '/files/etc/my.cnf/target[. = "mysqld"]',
+    changes => "set $param $value",
+    notify  => Service['mysqld'],
+  }
 }
